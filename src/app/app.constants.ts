e2c3ae3989b5e5d0923d562a787fac1800d78c35
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-07-26 16:19:13 -0400

    refactor

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,4 +2,4 @@ import { isDevMode } from "@angular/core";
 
 export const BRAND = 'FlyAirlines';
 
-export const API_URL = isDevMode() ? 'http://localhost:3000' : 'https://iota-airlines-api.vercel.app';
+export const API_URL = isDevMode() ? 'http://localhost:3000' : 'https://flyairlines-api.vercel.app';
