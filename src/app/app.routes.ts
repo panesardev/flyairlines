c29202cd545aa691952b0b4560e62c0c33099216
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-07-26 14:25:42 -0400

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { TitleResolver } from './shared/resolvers/title.resolver';
 
 export const routes: Routes = [
-  
+  {
+    path: '',
+    component: IndexComponent,
+    title: TitleResolver,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/',
+  }
 ];
